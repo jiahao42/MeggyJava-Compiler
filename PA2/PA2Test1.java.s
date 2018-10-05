68350e3279eb81f02a0c45a63dffc63cc49f487b
Jiahao Cai <caterpillarous@gmail.com>
2018-10-05 17:51:28 -0400

    update makefile and README

--- PA2/PA2Test1.java.s
+++ PA2/PA2Test1.java.s
@@ -19,6 +19,20 @@ main:
     /* Need to call this so that the meggy library gets set up */
 
 
+# Load constant int 1
+ldi    r24,lo8(1)
+ldi    r25,hi8(1)
+# push two byte expression onto stack
+push   r25
+push   r24
+
+# Casting int to byte by popping
+# 2 bytes off stack and only pushing low order bits
+# back on.  Low order bits are on top of stack.
+pop    r24
+pop    r25
+push   r24
+
 # Load constant int 2
 ldi    r24,lo8(2)
 ldi    r25,hi8(2)
@@ -26,102 +40,15 @@ ldi    r25,hi8(2)
 push   r25
 push   r24
 
-# Load constant int 3
-ldi    r24,lo8(3)
-ldi    r25,hi8(3)
-# push two byte expression onto stack
-push   r25
+# Casting int to byte by popping
+# 2 bytes off stack and only pushing low order bits
+# back on.  Low order bits are on top of stack.
+pop    r24
+pop    r25
 push   r24
 
 # Color expression Meggy.Color.YELLOW
 ldi    r22,3
-# push one byte expression onto stack
-push   r22
-
-### Meggy.setPixel(x,y,color) call
-# load a one byte expression off stack
-pop    r20
-# load a one byte expression off stack
-pop    r22
-# load a one byte expression off stack
-pop    r24
-call   _Z6DrawPxhhh
-call   _Z12DisplaySlatev
-
-# Load constant int 2
-ldi    r24,lo8(2)
-ldi    r25,hi8(2)
-# push two byte expression onto stack
-push   r25
-push   r24
-
-# Load constant int 3
-ldi    r24,lo8(3)
-ldi    r25,hi8(3)
-# push two byte expression onto stack
-push   r25
-push   r24
-
-# Color expression Meggy.Color.RED
-ldi    r22,1
-# push one byte expression onto stack
-push   r22
-
-### Meggy.setPixel(x,y,color) call
-# load a one byte expression off stack
-pop    r20
-# load a one byte expression off stack
-pop    r22
-# load a one byte expression off stack
-pop    r24
-call   _Z6DrawPxhhh
-call   _Z12DisplaySlatev
-
-# Load constant int 2
-ldi    r24,lo8(2)
-ldi    r25,hi8(2)
-# push two byte expression onto stack
-push   r25
-push   r24
-
-# Load constant int 3
-ldi    r24,lo8(3)
-ldi    r25,hi8(3)
-# push two byte expression onto stack
-push   r25
-push   r24
-
-# Color expression Meggy.Color.ORANGE
-ldi    r22,2
-# push one byte expression onto stack
-push   r22
-
-### Meggy.setPixel(x,y,color) call
-# load a one byte expression off stack
-pop    r20
-# load a one byte expression off stack
-pop    r22
-# load a one byte expression off stack
-pop    r24
-call   _Z6DrawPxhhh
-call   _Z12DisplaySlatev
-
-# Load constant int 2
-ldi    r24,lo8(2)
-ldi    r25,hi8(2)
-# push two byte expression onto stack
-push   r25
-push   r24
-
-# Load constant int 3
-ldi    r24,lo8(3)
-ldi    r25,hi8(3)
-# push two byte expression onto stack
-push   r25
-push   r24
-
-# Color expression Meggy.Color.WHITE
-ldi    r22,7
 # push one byte expression onto stack
 push   r22
 
--- PA2/README.md
+++ PA2/README.md
@@ -7,4 +7,15 @@
 
 ## Info about PA2
 
+Dear Mary and TA,
+
+If you have installed GNU Make, then you can use my `makefile` to simplify your work.
+
+* `make --file=my_makefile`: You can compile all the Java code to AVR Assembly and run them with MJSIM.jar at one time.
+
+* `make --file=my_makefile clean`: you can remove all the generated files (.s).
+
+I hope it can reduce your work :)
+
+-Jiahao
 
