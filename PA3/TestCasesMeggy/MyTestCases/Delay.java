3732d695b225de7fe5abe954f57e0381ddd3af56
Jiahao Cai <caterpillarous@gmail.com>
2018-10-22 22:46:12 -0400

    add test case for delay(), fix bug in Jcup

---
+++ PA3/TestCasesMeggy/MyTestCases/Delay.java
@@ -0,0 +1,22 @@
+/** 
+ *
+ * @File:      If
+ * @Brief:     Test syntax for Delay
+ * @Created:   Oct/20/2018
+ * @Author:    Jiahao Cai
+ *
+ */
+
+
+import meggy.Meggy;
+
+class Delay {
+	public static void main(String[] whatever) {
+		Meggy.delay(1);
+		Meggy.delay(10);
+		Meggy.delay(100);
+		Meggy.delay(1000);
+		Meggy.delay(10000);
+		Meggy.delay(99);
+	}
+}
