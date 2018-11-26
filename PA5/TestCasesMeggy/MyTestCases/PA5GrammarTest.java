16064ea19cf8a50d0b5dad9f5a9b4b7b5c378cf3
Jiahao Cai <caterpillarous@gmail.com>
2018-11-26 01:07:22 -0500

    fix bugs in mj.cup, add PA5 grammar test

---
+++ PA5/TestCasesMeggy/MyTestCases/PA5GrammarTest.java
@@ -0,0 +1,47 @@
+/** 
+ *
+ * @File:      PA5GrammarTest
+ * @Brief:     To test PA5 Grammar
+ * @Created:   Nov/25/2018
+ * @Author:    Jiahao Cai
+ *
+ */
+
+import meggy.Meggy;
+
+class PA5GrammarTest {
+  public static void main(String[] parameters) {
+    
+  }
+}
+
+class CustomType {
+  
+}
+
+class TestVarDecl {
+  // var declaration under class
+  int a;
+  byte b;
+  boolean c;
+  Meggy.Color color;
+  Meggy.Button bt;
+  Meggy.Tone tone;
+  TestVarDecl t; // class type
+  public int func(CustomType yo) {
+    int aa;
+    byte bb;
+    boolean cc;
+    Meggy.Color ccolor;
+    Meggy.Button bbtt;
+    Meggy.Tone ttone;
+    TestVarDecl tt; // class type
+    aa = 1;
+    bb = (byte)1;
+    cc = true;
+    ccolor = Meggy.Color.GREEN;
+    // bbtt = Meggy.Button.Up;
+    ttone = Meggy.Tone.Cs3;
+    return aa;
+  }
+}
