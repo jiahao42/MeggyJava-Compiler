41bab830a23ff01da81339b8dd6e81bbfce2db0e
Jiahao Cai <caterpillarous@gmail.com>
2018-12-02 14:41:09 -0500

    reformat some files

--- PA5/src/exceptions/InternalException.java
+++ PA5/src/exceptions/InternalException.java
@@ -1,14 +1,10 @@
 package exceptions;
 
-public class InternalException
-      extends RuntimeException
-{
-  public InternalException(String msg)
-  {
+public class InternalException extends RuntimeException {
+  public InternalException(String msg) {
     super(msg);
 
-    if(msg == null)
-    {
+    if (msg == null) {
       throw new InternalException("unexpected null argument");
     }
   }
--- PA5/src/exceptions/SemanticException.java
+++ PA5/src/exceptions/SemanticException.java
@@ -1,23 +1,18 @@
 package exceptions;
 
-public class SemanticException
-      extends RuntimeException
-{
-  public SemanticException(String msg, int line, int pos)
-  {
+public class SemanticException extends RuntimeException {
+  public SemanticException(String msg, int line, int pos) {
     super("[" + line + "," + pos + "] " + msg);
 
-    if(msg == null)
-    {
+    if (msg == null) {
       throw new InternalException("unexpected null argument");
     }
   }
-  public SemanticException(String msg)
-  {
+
+  public SemanticException(String msg) {
     super(msg);
 
-    if(msg == null)
-    {
+    if (msg == null) {
       throw new InternalException("unexpected null argument");
     }
   }
